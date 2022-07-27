cf73e2ad0e6b5d8a59540ca7289e1ee0325faff9
vinodkumar <vinodkumar4b9@gmail.com>
2022-07-27 21:39:56 +0530

    added

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-module "network" {
-  source = "./modules/network/main.tf"
-
-  bastion_instance_type = var.bastion_instance_type
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-047a51fa27710816e" # Amazon Linux
+  instance_type = "t2.micro"
+  count         = 5
+  tags = {
+    "Name" = "web"
+    #"Name" = "web-${count.index}"
+  }
 }
