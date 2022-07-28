29eb80e6d312e794a4b2aa58089d214ba18dd4ad
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2022-07-28 13:13:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "var.ami_id" # Amazon Linux
-  instance_type = "var.instance_type"
-  count         = "var.instance_count"
+  ami           = var.ami_id # Amazon Linux
+  instance_type = var.instance_type
+  count         = var.instance_count
   tags = {
     "Name" = "web"
     #"Name" = "web-${count.index}"
